7b5bb0f6e7aafe30639f2f75e98372ac89128932
Varun Sunil Bhojane <139360967+BHOJANE-VARUN@users.noreply.github.com>
2025-01-29 22:43:01 +0530

    Update database.js

--- src2/util/database.js
+++ src2/util/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
 const connectDB = async ()=>{
-   await mongoose.connect("mongodb+srv://varunbhojane07:Varunbhojane07@varun-cluster.cv4xb.mongodb.net/Iron_bloods");
+   await mongoose.connect("mongodb+srv://cv4xb.mongodb.net/Iron_bloods");
 }
 module.exports = connectDB;
